832d182c797707d08925e5690d710145de69ecf4
Hevon Gordon <hevonqw@qualityworkss-MacBook-Pro.local>
2018-11-27 23:31:28 -0500

    added sidebar component

---
+++ app/src/components/Sidebar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="sidebar">
+        <div class="row col-sm-12 col-lg-12 col-md-12" v-for="(row, index) in rows" :key=index>
+            <div class="col-sm-4 col-md-4 col-lg-4 icon">
+                <img :src="require(`@/assets/${row.icon}`)" alt="x">
+            </div>
+            <div class="offset-md-1 offset-lg-1 offset-sm-1 col-sm-6 col-md-6 col-lg-6 title">{{row.title}}</div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    data() {
+        return {
+            rows: [
+                {icon: 'note.svg', title: 'Notes'},
+                {icon: 'bin.svg', title: 'Trash'},
+            ],
+        };
+    },
+});
+</script>
+<style scoped>
+div.row{
+    top: 40px;
+    padding-bottom:20px;
+   font-family: 'Montserrat', sans-serif;
+   cursor: pointer;
+}
+div.sidebar{
+    height: 100%;
+    width: 125px;
+    background-color:#2e3235;
+}
+div.icon, div.title{
+    color: white;
+
+}
+div.icon img{
+    width:20px;
+    height: 20px;
+}
+
+</style>
+
+
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<div class="main-container">
+  <Sidebar></Sidebar>
+</div>
+  
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from 'vue';
+import Sidebar from '@/components/Sidebar.vue'; // @ is an alias to /src
 
-@Component({
+export default Vue.extend({
+  name: 'home',
   components: {
-    HelloWorld,
+    Sidebar,
   },
-})
-export default class Home extends Vue {}
+});
 </script>
+<style>
+div.main-container{
+  height: 100vh;
+}
+</style>
+
