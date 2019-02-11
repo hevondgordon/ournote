c00f4c5f06523f29172c2e9de30e0e3a181f5f5b
hevongordon <hgordon@qualityworkscg.com>
2019-02-10 20:05:06 -0500

    updated store with new state 'activeNote' along with actions mutations

--- app/src/store.ts
+++ app/src/store.ts
@@ -5,12 +5,19 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    activeNote: '',
   },
   mutations: {
-
+    updateActiveNote(state, note) {
+      state.activeNote = note;
+    },
   },
   actions: {
-
+    updateActiveNote({ commit }, note) {
+      commit('updateActiveNote');
+    },
+  },
+  getters: {
+    getActiveNote: (state) => state.activeNote,
   },
 });
