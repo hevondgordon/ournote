c208958917460d1f12b0ef9fab84c889aa7e3319
hevongordon <hgordon@qualityworkscg.com>
2019-02-11 00:33:06 -0500

    added search functionality

--- app/src/components/Search.vue
+++ app/src/components/Search.vue
@@ -1,212 +1,200 @@
 <template>
-    <div class="search">
-        <input type="text" name="search" id="search">
-    </div>
+  <div class="search">
+    <input type="text" name="search" id="search" v-model="searchInput" @keyup="search">
+  </div>
 </template>
-<script lang="ts">
+<script lang='ts'>
 import Vue from 'vue';
 export default Vue.extend({
-    data() {
-        return {
-
-        };
+  data() {
+    return {
+      searchInput: '',
+    };
+  },
+  methods: {
+    search(event: any) {
+      if (this.$data.searchInput === '') {
+          if (this.$store.state.searching) {
+            this.$store.dispatch('disableSearch');
+          }
+      } else {
+        this.$store.dispatch('updateSearchResults', this.$data.searchInput);
+      }
     },
+  },
 });
 </script>
 <style>
 /* ----------- Non-Retina Screens ----------- */
-@media screen 
-  and (min-device-width: 1200px) 
-  and (max-device-width: 1600px) 
-  and (-webkit-min-device-pixel-ratio: 1) { 
-      div.search{
-            padding-bottom: 30px;
-            padding-top:20px;
-            padding-left:10px;
-            width: 100%;
-        }
-        input#search {
-            border-radius: 4px;
-            border: 1px solid lightgray;
-            height: 35px;
-            width:100%;
-            padding: 6px;
-            font-size: 14px;
-        }
+@media screen and (min-device-width: 1200px) and (max-device-width: 1600px) and (-webkit-min-device-pixel-ratio: 1) {
+  div.search {
+    padding-bottom: 30px;
+    padding-top: 20px;
+    padding-left: 10px;
+    width: 100%;
+  }
+  input#search {
+    border-radius: 4px;
+    border: 1px solid lightgray;
+    height: 35px;
+    width: 100%;
+    padding: 6px;
+    font-size: 14px;
+  }
 }
 
 /* ----------- Retina Screens ----------- */
-@media screen 
-  and (min-device-width: 1200px) 
-  and (max-device-width: 1600px) 
-  and (-webkit-min-device-pixel-ratio: 2)
-  and (min-resolution: 192dpi) { 
-      div.search{
-            padding-bottom: 30px;
-            padding-top:20px;
-            padding-left:10px;
-            width: 100%;
-        }
-        input#search {
-            border-radius: 4px;
-            border: 1px solid lightgray;
-            height: 35px;
-            width:100%;
-            padding: 6px;
-            font-size: 14px;
-        }
+@media screen and (min-device-width: 1200px) and (max-device-width: 1600px) and (-webkit-min-device-pixel-ratio: 2) and (min-resolution: 192dpi) {
+  div.search {
+    padding-bottom: 30px;
+    padding-top: 20px;
+    padding-left: 10px;
+    width: 100%;
+  }
+  input#search {
+    border-radius: 4px;
+    border: 1px solid lightgray;
+    height: 35px;
+    width: 100%;
+    padding: 6px;
+    font-size: 14px;
+  }
 }
 /* ----------- iPhone 4 and 4S ----------- */
 
 /* Portrait and Landscape */
-@media only screen 
-  and (min-device-width: 320px) 
-  and (max-device-width: 480px){
-      div.search{
-            padding-bottom: 30px;
-            padding-top:20px;
-            padding-left:25px;
-            width: 100%;
-        }
-        input#search {
-            outline:none;
-            border-radius: 4px;
-            border: 1px solid lightgray;
-            height: 35px;
-            width:100%;
-            padding: 6px;
-            font-size: 14px;
-        }
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+  div.search {
+    padding-bottom: 30px;
+    padding-top: 20px;
+    padding-left: 25px;
+    width: 100%;
+  }
+  input#search {
+    outline: none;
+    border-radius: 4px;
+    border: 1px solid lightgray;
+    height: 35px;
+    width: 100%;
+    padding: 6px;
+    font-size: 14px;
+  }
 }
 /* ----------- iPhone 5, 5S, 5C and 5SE ----------- */
 
 /* Portrait and Landscape */
-@media only screen 
-  and (min-device-width: 320px) 
-  and (max-device-width: 568px){
-      div.search{
-            padding-bottom: 30px;
-            padding-top:20px;
-            padding-left:25px;
-            width: 100%;
-        }
-        input#search {
-            outline:none;
-            border-radius: 4px;
-            border: 1px solid lightgray;
-            height: 35px;
-            width:100%;
-            padding: 6px;
-            font-size: 14px;
-        }
+@media only screen and (min-device-width: 320px) and (max-device-width: 568px) {
+  div.search {
+    padding-bottom: 30px;
+    padding-top: 20px;
+    padding-left: 25px;
+    width: 100%;
+  }
+  input#search {
+    outline: none;
+    border-radius: 4px;
+    border: 1px solid lightgray;
+    height: 35px;
+    width: 100%;
+    padding: 6px;
+    font-size: 14px;
+  }
 }
 /* ----------- iPhone 6, 6S, 7 and 8 ----------- */
 
 /* Portrait and Landscape */
-@media only screen 
-  and (min-device-width: 375px) 
-  and (max-device-width: 667px) { 
-      div.search{
-            padding-bottom: 30px;
-            padding-top:20px;
-            padding-left:25px;
-            width: 100%;
-        }
-        input#search {
-            outline:none;
-            border-radius: 4px;
-            border: 1px solid lightgray;
-            height: 35px;
-            width:100%;
-            padding: 6px;
-            font-size: 14px;
-        }
+@media only screen and (min-device-width: 375px) and (max-device-width: 667px) {
+  div.search {
+    padding-bottom: 30px;
+    padding-top: 20px;
+    padding-left: 25px;
+    width: 100%;
+  }
+  input#search {
+    outline: none;
+    border-radius: 4px;
+    border: 1px solid lightgray;
+    height: 35px;
+    width: 100%;
+    padding: 6px;
+    font-size: 14px;
+  }
 }
 /* ----------- iPhone 6+, 7+ and 8+ ----------- */
 
 /* Portrait and Landscape */
-@media only screen 
-  and (min-device-width: 414px) 
-  and (max-device-width: 736px) { 
-      div.search{
-            padding-bottom: 30px;
-            padding-top:20px;
-            padding-left:25px;
-            width: 100%;
-        }
-        input#search {
-            outline:none;
-            border-radius: 4px;
-            border: 1px solid lightgray;
-            height: 35px;
-            width:100%;
-            padding: 6px;
-            font-size: 14px;
-        }
+@media only screen and (min-device-width: 414px) and (max-device-width: 736px) {
+  div.search {
+    padding-bottom: 30px;
+    padding-top: 20px;
+    padding-left: 25px;
+    width: 100%;
+  }
+  input#search {
+    outline: none;
+    border-radius: 4px;
+    border: 1px solid lightgray;
+    height: 35px;
+    width: 100%;
+    padding: 6px;
+    font-size: 14px;
+  }
 }
 /* ----------- iPhone X ----------- */
 
 /* Portrait and Landscape */
-@media only screen 
-  and (min-device-width: 375px) 
-  and (max-device-width: 812px) { 
-    div.search{
-            padding-bottom: 30px;
-            padding-top:20px;
-            padding-left:25px;
-            width: 100%;
-        }
-        input#search {
-            outline:none;
-            border-radius: 4px;
-            border: 1px solid lightgray;
-            height: 35px;
-            width:100%;
-            padding: 6px;
-            font-size: 14px;
-        }
+@media only screen and (min-device-width: 375px) and (max-device-width: 812px) {
+  div.search {
+    padding-bottom: 30px;
+    padding-top: 20px;
+    padding-left: 25px;
+    width: 100%;
+  }
+  input#search {
+    outline: none;
+    border-radius: 4px;
+    border: 1px solid lightgray;
+    height: 35px;
+    width: 100%;
+    padding: 6px;
+    font-size: 14px;
+  }
 }
-@media screen 
-  and (max-device-width: 320px) 
-  and (max-device-height: 640px) {
-    div.search{
-            padding-bottom: 30px;
-            padding-top:20px;
-            padding-left:25px;
-            width: 100%;
-        }
-        input#search {
-            outline:none;
-            border-radius: 4px;
-            border: 1px solid lightgray;
-            height: 35px;
-            width:100%;
-            padding: 6px;
-            font-size: 14px;
-        }
+@media screen and (max-device-width: 320px) and (max-device-height: 640px) {
+  div.search {
+    padding-bottom: 30px;
+    padding-top: 20px;
+    padding-left: 25px;
+    width: 100%;
+  }
+  input#search {
+    outline: none;
+    border-radius: 4px;
+    border: 1px solid lightgray;
+    height: 35px;
+    width: 100%;
+    padding: 6px;
+    font-size: 14px;
+  }
 }
 
-@media screen 
-  and (max-device-width: 360px) 
-  and (max-device-height: 640px) {
-    div.search{
-            padding-bottom: 30px;
-            padding-top:20px;
-            padding-left:25px;
-            width: 100%;
-        }
-        input#search {
-            outline:none;
-            border-radius: 4px;
-            border: 1px solid lightgray;
-            height: 35px;
-            width:100%;
-            padding: 6px;
-            font-size: 14px;
-        }
+@media screen and (max-device-width: 360px) and (max-device-height: 640px) {
+  div.search {
+    padding-bottom: 30px;
+    padding-top: 20px;
+    padding-left: 25px;
+    width: 100%;
+  }
+  input#search {
+    outline: none;
+    border-radius: 4px;
+    border: 1px solid lightgray;
+    height: 35px;
+    width: 100%;
+    padding: 6px;
+    font-size: 14px;
+  }
 }
-
-
 </style>
 
 
--- app/src/store.ts
+++ app/src/store.ts
@@ -6,18 +6,62 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     activeNote: '',
+    searching: false,
+    searchResults: [{
+      noteId: 0,
+      note: '',
+      timestamp: 0,
+    }],
+    notes: [
+      {
+        noteId: 1,
+        note: 'hello world 1',
+        timestamp: 1009873,
+      },
+      {
+        noteId: 2,
+        note: 'hey hello there id 2',
+        timestamp: 1009873,
+      },
+      {
+        noteId: 3,
+        note: 'this is a note with id 3',
+        timestamp: 1009873,
+      },
+      {
+        noteId: 4,
+        note: 'swimming hello id 4',
+        timestamp: 1009873,
+      },
+    ],
   },
   mutations: {
     updateActiveNote(state, note) {
       state.activeNote = note;
     },
+    updateSearchResults(state, searchInput) {
+      state.searching = true;
+      const searchResults = state.notes.filter((item) => item.note.match(searchInput));
+      console.log(searchResults);
+      state.searchResults = searchResults;
+    },
+    disableSearch(state) {
+      state.searching = false;
+    },
   },
   actions: {
+    disableSearch({commit}) {
+      commit('disableSearch');
+    },
+    updateSearchResults({ commit }, searchInput: string) {
+      commit('updateSearchResults', searchInput);
+    },
     updateActiveNote({ commit }, note) {
       commit('updateActiveNote');
     },
   },
   getters: {
     activeNote: (state) => state.activeNote,
+    searchStatus: (state) => state.searching,
   },
 });
